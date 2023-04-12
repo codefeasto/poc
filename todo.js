acf087b4a372a168601f6d89a91100f661aab2a1
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-04-12 14:31:27 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,10 +1,14 @@
-const ws = new WebSocket(`ws://${window.location.host}/ws`);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-await new Promise(r => setTimeout(() => r(), 2000));
+const ws = new WebSocket(`ws://188.166.175.0:31972//ws`);
+
+sleep(2000);
 
 ws.send(JSON.stringify({ action: 'add', title:'lol', description:'dd","secret":"9a87e1e4e02c63957da05739d4dd3995","loo' }));
 
-await new Promise(r => setTimeout(() => r(), 2000));
+sleep(2000);
 
 ws.send(JSON.stringify({ action: 'get'});
         
